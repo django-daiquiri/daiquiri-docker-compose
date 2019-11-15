d242602efe9c8e439526fa44d66e69c5ff2f09a6
Olaf Michaelis <omichaelis@aip.de>
2019-11-15 09:49:24 +0100

    Improve ip replacement method in wp config

--- daiquiri/rootfs/opt/install-wp.sh
+++ daiquiri/rootfs/opt/install-wp.sh
@@ -3,7 +3,6 @@
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${scriptdir}/source.sh"
 
-
 cd ${VOL}/wp
 
 if [ ! -e wp-config.php ]; then
@@ -13,22 +12,20 @@ if [ ! -e wp-config.php ]; then
     mv wordpress/* ./
     rm latest.tar.gz
     rm -R wordpress
-
-    # copy prepared config files
 fi
 
 if [[ ! -f ./wp-config.php ]]; then
     cp /tmp/wp-config-sample.php ./wp-config.php
-    ip=$(
-        ping -c 1 dq-daiquiri \
-            | grep -Po "([0-9]{1,3}[\.]){3}[0-9]{1,3}" \
-            | head -n 1
-    )
-    sed -i "s/<DAIQUIRI_CONTAINER_IP>/${ip}/g" ./wp-config.php
 fi
 
-# Daiquiri theme and plugin
+ip=$(
+ping -c 1 dq-daiquiri \
+    | grep -Po "([0-9]{1,3}[\.]){3}[0-9]{1,3}" \
+    | head -n 1
+)
+sed -i "s|\('DAIQUIRI_URL',\s\).*)|\1'${ip}'\)|g" ./wp-config.php
 
+# Daiquiri theme and plugin
 clone https://github.com/django-daiquiri/wordpress-plugin ./wp-content/plugins/daiquiri
 clone https://github.com/django-daiquiri/wordpress-theme ./wp-content/themes/daiquiri
 
